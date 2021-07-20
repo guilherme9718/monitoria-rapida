879c8195e9ec6de888df622d6cd7bcd1d60edb1c
Vitor Triches <87033099+vitorrutfpr@users.noreply.github.com>
2021-07-20 19:03:09 -0300

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include "Senha.h"
+#include "cadastro.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    this->ra = "";
     ui->setupUi(this);
+    MainWindow::setWindowTitle("Login");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+   ra = ui->lineEdit->text();
+   if(ra_cadastrado(ra)){
+       hide();
+       Senha* senha = new Senha(this);
+       senha->show();
+   }
+   else{
+       hide();
+       Cadastro* cadastro = new Cadastro(this);
+       cadastro->show();
+   }
+}
+
+bool MainWindow::ra_cadastrado(QString r){
+    //retorna 1 se está no sistema 0 se nao
+    return 0;
+}
